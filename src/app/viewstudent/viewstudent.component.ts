8551137d67f8d966b814147b583814f1be5eb0cb
Fahim-16 <fahim.sakeer@gmail.com>
2022-07-08 11:59:47 +0530

    student view created

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -11,5 +11,12 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-name="Riya"
+data=[
+  {"name":"riya","age":20,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"},
+{"name":"nisha","age":21,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"},
+{"name":"miya","age":22,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"},
+{"name":"athul","age":23,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"},
+{"name":"nikhil","age":24,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"},
+{"name":"gautham","age":25,"photo":"https://tse4.mm.bing.net/th?id=OIP.sVVB9SERkuOQQNu3XP2uFAHaJi&pid=Api&P=0&w=129&h=167"}
+]
 }
